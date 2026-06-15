ca1f2de03e1903e47b234b60a26ce228e178d243
ShoxaMars <11072011shoh@gmail.com>
2026-06-15 17:42:52 +0500

    project start

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-red-600 text-white text -7xl text-center'></h1>
+    </div>
+  )
+}
+
+export default App
